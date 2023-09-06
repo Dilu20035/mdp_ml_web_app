44c88d4383954dcb02438ca607e2eeef35cc65b5
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-09-06 12:35:27 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 22 11:53:51 2022
+Created on Sun May  8 21:01:15 2022
 
 @author: siddhardhan
 """
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/Jilshad M/Desktop/MDP Web ML/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/Jilshad M/Desktop/MDP Web ML/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/Jilshad M/Desktop/MDP Web ML/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
@@ -242,4 +242,5 @@ if (selected == "Parkinsons Prediction"):
           parkinsons_diagnosis = "The person does not have Parkinson's disease"
         
     st.success(parkinsons_diagnosis)
+
 
