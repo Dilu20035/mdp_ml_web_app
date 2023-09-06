ce0e00e78bd6ec01a0f752f3434e2e1e629514f5
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-09-06 12:26:12 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -12,7 +12,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:\Users\Jilshad M\Desktop\MDP Web ML\diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('C:/Users/Jilshad M/Desktop/MDP Web ML/diabetes_model.sav', 'rb'))
 
 heart_disease_model = pickle.load(open('C:\Users\Jilshad M\Desktop\MDP Web ML\heart_disease_model.sav', 'rb'))
 
