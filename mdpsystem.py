6e286682fe32e89ea1828dc940817e8be767a538
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-09-07 21:10:30 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -6,17 +6,6 @@ from streamlit_option_menu import option_menu
 
 
 # loading the saved models
-
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
-
-heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
-
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
-
-
-
-# sidebar for navigation
-with st.sidebar:
     st.set_page_config(
     page_title="MDP-Detector",
     page_icon="🧊",
@@ -28,6 +17,17 @@ with st.sidebar:
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
+
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+
+
+
+# sidebar for navigation
+with st.sidebar:
     
     selected = option_menu('Multiple Disease Detection System',
                           
@@ -35,7 +35,7 @@ with st.sidebar:
                               'Diabetes Prediction',
                            'Heart Disease Prediction',
                            'Parkinsons Prediction'],
-                          icons=['⌂','activity','heart','person'],
+                          icons=['home','activity','heart','person'],
                           default_index=0)
     
 #Home page
