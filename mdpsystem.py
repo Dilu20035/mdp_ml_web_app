d007fe6df540181a0e6cf493f7f232738e2edcfa
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-10-22 09:49:54 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -31,9 +31,30 @@ st.set_page_config(
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
+
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 
+def get_user_name():
+    return 'John'
+
+# ------------------------------------------------
+# Want people to see this part of the code...
+
+def get_punctuation():
+    return '!!!'
+
+greeting = "Hi there, "
+user_name = get_user_name()
+punctuation = get_punctuation()
+
+st.write(greeting, user_name, punctuation)
+
+# ...up to here
+# ------------------------------------------------
+
+foo = 'bar'
+st.write('Done!')
 
 
 # sidebar for navigation
