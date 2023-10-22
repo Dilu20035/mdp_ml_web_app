01faff2e8b71468f0037762a68750743499fb169
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-10-22 09:54:31 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -35,26 +35,11 @@ st.set_page_config(
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 
-def get_user_name():
-    return 'John'
-
-# ------------------------------------------------
-# Want people to see this part of the code...
-
-def get_punctuation():
-    return '!!!'
-
-greeting = "Hi there, "
-user_name = get_user_name()
-punctuation = get_punctuation()
-
-st.write(greeting, user_name, punctuation)
-
-# ...up to here
-# ------------------------------------------------
-
-foo = 'bar'
-st.write('Done!')
+# Greet the user by their name.
+if st.experimental_user.email:
+    # Get the user's name from the database.
+    name = get_name_from_db(st.experimental_user.email)
+    st.write('Hello, %s!' % name)
 
 # sidebar for navigation
 with st.sidebar:
