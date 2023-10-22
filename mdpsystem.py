d32254188e968f1f17c8818ed380a54d570ec87a
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-10-22 10:05:20 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -26,18 +26,13 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
     menu_items={
-        'Get Help': 'https://bgremoval.streamlit.app/',
+        'Click Help': 'https://bgremoval.streamlit.app/',
         'Report a bug': "https://www.extremelycoolapp.com/bug",
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
 
 
 # sidebar for navigation
