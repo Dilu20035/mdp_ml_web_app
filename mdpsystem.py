b405b1182b40a6f4a80b6f1a2355c01eac741513
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-10-22 09:47:30 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -26,7 +26,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Get Help': 'https://bgremoval.streamlit.app/',
         'Report a bug': "https://www.extremelycoolapp.com/bug",
         'About': "# This is a header. This is an *extremely* cool app!"
     }
