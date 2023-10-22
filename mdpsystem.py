623cea0cc702fecfe72325ed861ed2d3b84d3945
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2023-10-22 10:24:09 +0530

    Update mdpsystem.py

--- mdpsystem.py
+++ mdpsystem.py
@@ -20,6 +20,8 @@ parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 covid_model = pickle.load(open("covid.pkl", 'rb'))
 
+
+st.set_option(client_toolbarMode="developer")
 st.set_page_config(
     page_title="Disease Detector",
     page_icon="🧊",
